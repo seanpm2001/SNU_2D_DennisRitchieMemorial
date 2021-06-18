58ac06f23f9f36ec7864284f41dd04d403aef55a
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-06-17 19:32:45 -0700

    8 sub-directories (91 files)
    8 sub-directories for this directory, containing 91 files in various formats.

---
+++ External/Research/C_ProgrammingLanguage/Samples/LONG.c
@@ -0,0 +1,15 @@
+long some_function();
+/* int */ other_function();
+
+/* int */ calling_function()
+{
+    long test1;
+    register /* int */ test2;
+
+    test1 = some_function();
+    if (test1 > 0)
+          test2 = 0;
+    else
+          test2 = other_function();
+    return test2;
+}
